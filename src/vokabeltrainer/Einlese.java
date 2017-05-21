07780a6a86f56b95c6817c9d8ccb0c4d9030fc77
Friedrich <Friedrich@LAPTOP-1TC9FOLN.mshome.net>
2017-05-21 20:25:59 +0200

    Anfänge der Einlese

--- src/vokabeltrainer/Einlese.java
+++ src/vokabeltrainer/Einlese.java
@@ -1,6 +1,30 @@
 package vokabeltrainer;
 
+import java.io.File;
 import java.io.Serializable;
+import javax.swing.JFileChooser;
 
 public class Einlese implements Serializable {
+
+    private File input;
+
+    public Einlese() {
+    }
+
+    public void oeffnen() {
+        JFileChooser fc = new JFileChooser();
+        String pathOfFile = null;
+        fc.setVisible(true);
+        int returnVal = fc.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            pathOfFile = fc.getSelectedFile().getAbsolutePath();
+            input = fc.getSelectedFile();
+        }
+        einLesen(pathOfFile);
+    }
+
+    private void einLesen(String pathOfFile) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
