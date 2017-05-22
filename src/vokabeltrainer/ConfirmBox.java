a8606e8bd94d86357acec608503f25f803a4c924
Rektaszension <manuel.eberts@gmail.com>
2017-05-22 20:00:52 +0200

    Add files via upload

---
+++ src/vokabeltrainer/ConfirmBox.java
@@ -0,0 +1,36 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ConfirmBox{
+
+	private static Boolean antwort;
+	public static boolean anzeigen(){
+		Stage secundaryStage = new Stage();
+		secundaryStage.initModality(Modality.APPLICATION_MODAL);
+		Button yesButton = new Button("Ja   ");
+		yesButton.setOnAction(e -> {
+			antwort = true;
+			secundaryStage.close();
+		});
+		Button noButton = new Button("Nein");
+		noButton.setOnAction(e -> {
+			antwort = false;
+			secundaryStage.close();
+		});
+		Label label = new Label("Wollen sie das Programm wirklich beenden?");
+		
+		
+		VBox layout = new VBox();
+		layout.getChildren().addAll(label,yesButton,noButton);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(layout,300,300);
+		secundaryStage.setScene(scene);
+		secundaryStage.showAndWait();
+		return (antwort);
+	}
+}
---
+++ src/vokabeltrainer/ViewVokTrainer.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ViewVokTrainer extends Application
+{ 	
+	public static void main(String[] args){
+		launch(args);
+	}
+	
+    public void start(Stage primaryStage) throws Exception{
+    		primaryStage.setOnCloseRequest(e->{
+    			e.consume();
+    			applicationBeenden();
+    		});
+		StackPane layout = new StackPane();
+		Scene scene = new Scene(layout, 1000, 1000);
+		primaryStage.setTitle("Vokabeltrainer");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+    
+    public void applicationBeenden(){
+    		Boolean antwort = ConfirmBox.anzeigen();
+    		if(antwort){
+    			Platform.exit();
+    		}
+    }
+}
