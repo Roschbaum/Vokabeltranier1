93036f7b728df3d0e2608bffb52d819c14f2aba4
Friedrich <Friedrich@LAPTOP-1TC9FOLN.mshome.net>
2017-05-25 18:47:43 +0200

    Komentare

--- src/vokabeltrainer/VokabelErstellen.java
+++ src/vokabeltrainer/VokabelErstellen.java
@@ -6,45 +6,79 @@
 package vokabeltrainer;
 
 /**
+ * Erstellt die Eingabemöglichkeit und speichert die Eingaben die für die
+ * Erstellung einer Vokabel nötig sind.
  *
  * @author Friedrich
  */
 public class VokabelErstellen {
 
+    /**
+     * Erstellt ein Fenster mit den Eingabemöklichkeiten, Speichert diese,
+     * Registrierd ob die Eingabe richtig ist.
+     */
     public VokabelErstellen() {
     }
 
-    void closeVokabeleingabe() {
+    /**
+     * Schließt das Eingabefenster
+     */
+    public void closeVokabeleingabe() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void erstelleVokabeleingabe() {
+    /**
+     * Gibt die Eingabe zurück, ob die neue Vokabel heufig ist.
+     *
+     * @return
+     */
+    public boolean getBooleanHeufig() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    boolean getBooleanHeufig() {
+    /**
+     * Gibt wieder ob die neue Vokabel sich in einem Vokabelkasten befindet.
+     *
+     * @return
+     */
+    public boolean getBooleanVokabelkasten() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    boolean getBooleanVokabelkasten() {
+    /**
+     * Gibt die Bedeutung der neuen Vokabel wieder.
+     *
+     * @return
+     */
+    public String getEingabeBedeutung() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    String getEingabeBedeutung() {
+    /**
+     * Gibt den Namen der neuen Vokabel wieder.
+     *
+     * @return
+     */
+    public String getEingabeName() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    String getEingabeName() {
+    /**
+     * Gibt zurück ob die Eingabe richtig verlaufen ist.
+     *
+     * @return
+     */
+    public boolean getEingabeRichtig() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    boolean getEingabeRichtig() {
+    /**
+     * Gibt die Zusatzangaen der neuen Vokabel wieder.
+     *
+     * @return
+     */
+    public String getEingabeZusatzangaben() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    String getEingabeZusatzangaben() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
 }
